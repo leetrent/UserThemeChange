68c3cd6a84c369742c4b37a308d392c3126d5cf5
LeeTrent <lee.trent.1@gmail.com>
2022-06-06 06:39:46 -0500

    Added UserPreferences controller and view

---
+++ UserThemeChange/Controllers/UserPreferencesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UserThemeChange.Controllers
+{
+    public class UserPreferencesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
